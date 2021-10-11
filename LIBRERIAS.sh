8f812c19eb92f9b527b316e7cbd3bc6d50fc37f9
AaronMartinezMtz <56774670+AaronMartinezMtz@users.noreply.github.com>
2021-10-11 09:35:08 -0500

    Add files via upload

---
+++ LIBRERIAS.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# No es que existan las librerías pero se puede simular # algo similar
+# Esta es la forma de importar funciones source libreria.sh
+muestrapantalla 69 123 "epa"
+sumame 1337 3389 && echo "OK" || echo "Ocurrió un error" echo "Resultado: ${RESULTADO} $!"
---
+++ SENIEALES.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+funcion () {
+echo "Se ha recibido una señal: ${FUNCNAME} ${0}" #exit
+}
+
+do
+sleep 2
+echo "ufff qué sueño..." done
